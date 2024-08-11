17398b747ecb4c732b66c57cdff80550e046621f
sheta <sheta.ahmed5@gmail.com>
2024-08-11 13:39:03 +0200

    Add generic object object class and functions
    Implemented functions to read and writing objects

--- libwyag.py
+++ libwyag.py
@@ -162,6 +162,87 @@ example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
       else:
         return None
     return repo_find(parent, required)
+  
+class GitObject(object):
+  
+  def __init__(self, data=None):
+    if data!=None:
+      self.deserialize(data)
+    else:
+      self.init()
+    
+    def serialize(self,repo):
+          
+      """This function MUST be implemented by subclasses.
+      It must read the object's contents from self.data, a byte string, and do
+      whatever it takes to convert it into a meaningful representation.  What exactly that means depend on each subclass."""
+      raise Exception("Unimplemented")
+    def deserialize(self,data):
+      raise Exception("Unimplemented")
+
+    def init(self):
+      pass
+
+    def object_read(repo, sha):
+      """Read object sha from Git repository repo.  Return a
+    GitObject whose exact type depends on the object."""
+      path =  repo_file(repo,"object",sha[0:2], sha[2:])
+
+      if not os.path.isfile(path):
+        return None
+      
+      with open (path,"rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size
+        y = raw.find(b'\x00',x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+          raise Exception(f"Malformed object {format(sha)}: bad length")
+        
+        # Pick constructor
+        match fmt:
+          case b'commit' : c=GitCommit
+          case b'tree' : c=GitTree
+          case b'tag' : c=GitTaf
+          case b'blob' : c=GitBlob
+          case _:
+            raise Exception(f"Unkown type {format(fmt.decode("ascii"))} for object {sha}")
+        
+        # Call constructor and return object
+        return c(raw[y+1:])
+      
+    def object_write(obj, repo=None):
+
+      # Serialize object data
+      data = obj.serialize()
+      # Add header
+      result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+      # Compute hash
+      sha = hashlib.sha1(result).hexdigest()
+
+      if repo:
+          # Compute path
+          path=repo_file(repo, "objects", sha[0:2], sha[2:], mkdir=True)
+
+          if not os.path.exists(path):
+              with open(path, 'wb') as f:
+                  # Compress and write
+                  f.write(zlib.compress(result))
+      return sha
+
+
+    
+  
+
+
+
+  
+
     
 
 
